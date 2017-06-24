35107b17cd3a82859c60672729178b68c4105c4d
Adam <adam@adamslair.net>
2017-06-24 20:05:37 +0200

    Move ANim

--- Game/Assets/Scripts/CharController.cs
+++ Game/Assets/Scripts/CharController.cs
@@ -10,7 +10,10 @@ namespace GameJam
 		private float moveSpeed = 5.0f;
 		[SerializeField]
 		private float jumpStrength = 1.0f;
+		[SerializeField]
+		private MeshRenderer sprite = null;
 
+		private Vector3 baseScale = Vector3.one;
 		private float targetMovement = 0.0f;
 		private Rigidbody2D body;
 
@@ -32,9 +35,23 @@ namespace GameJam
 		private void Awake()
 		{
 			this.body = this.GetComponent<Rigidbody2D>();
+			this.baseScale = this.sprite.transform.localScale;
 		}
 		private void Update()
 		{
+			if (Mathf.Abs(this.targetMovement) > 0.1f)
+			{
+				this.sprite.transform.localScale = new Vector3(
+					Mathf.MoveTowards(
+						this.sprite.transform.localScale.x,
+						this.baseScale.x * Mathf.Sign(this.targetMovement),
+						Mathf.Abs(this.baseScale.x) * Time.deltaTime),
+					this.baseScale.y,
+					this.baseScale.z);
+			}
+			this.sprite.transform.rotation = Quaternion.AngleAxis(
+				Mathf.Sin(Time.time * 3.0f) * 2.0f * Mathf.Abs(this.targetMovement), 
+				Vector3.back);
 			this.body.velocity = new Vector2(
 				this.targetMovement * this.moveSpeed, 
 				this.body.velocity.y);
