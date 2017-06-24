a5e165c919b71ae12dd06a2ac0a6e7a481fc1bf3
Adam <adam@adamslair.net>
2017-06-24 20:38:31 +0200

    rig siugsi fh lsih lshrglisugisugi lsurksugksrgksuh gl

---
+++ Game/Assets/Scripts/StoneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+	public class StoneTrigger : MonoBehaviour
+	{
+		private void OnCollisionEnter2D(Collision2D collision)
+		{
+			if (collision.gameObject.GetComponent<CharController>() == null)
+				return;
+
+			Debug.Log("Test");
+			Rigidbody2D body = this.GetComponent<Rigidbody2D>();
+			if (body.constraints != RigidbodyConstraints2D.None)
+			{
+				body.constraints = RigidbodyConstraints2D.None;
+				body.gameObject.layer = 10;
+			}
+		}
+	}
+}
