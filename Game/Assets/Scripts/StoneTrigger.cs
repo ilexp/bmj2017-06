f8c4f4a104ebc84cbbe37fdd176d0b6074dc5b0b
Adam <adam@adamslair.net>
2017-06-24 20:50:18 +0200

    adsas das dasdad asd asd asd a

--- Game/Assets/Scripts/StoneTrigger.cs
+++ Game/Assets/Scripts/StoneTrigger.cs
@@ -6,17 +6,30 @@ namespace GameJam
 {
 	public class StoneTrigger : MonoBehaviour
 	{
+		[SerializeField]
+		private GameObject[] activateObj;
+
+		private IEnumerable LateStuff()
+		{
+			yield return new WaitForSeconds(1.75f);
+			foreach (GameObject obj in activateObj)
+			{
+				obj.SetActive(true);
+			}
+		}
+
 		private void OnCollisionEnter2D(Collision2D collision)
 		{
 			if (collision.gameObject.GetComponent<CharController>() == null)
 				return;
 
-			Debug.Log("Test");
 			Rigidbody2D body = this.GetComponent<Rigidbody2D>();
 			if (body.constraints != RigidbodyConstraints2D.None)
 			{
 				body.constraints = RigidbodyConstraints2D.None;
 				body.gameObject.layer = 10;
+
+				StartCoroutine(LateStuff().GetEnumerator());
 			}
 		}
 	}
