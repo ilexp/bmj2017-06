0464aa47f278ef7201669a169a67e771fe0dd0d4
Adam <adam@adamslair.net>
2017-06-24 14:59:22 +0200

    Character Controller Test

---
+++ Game/Assets/Scripts/CharController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+	public class CharController : MonoBehaviour
+	{
+		private void Update()
+		{
+			Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+			float leftRight = Input.GetAxis("Horizontal");
+			body.velocity = new Vector2(leftRight * 5.0f, body.velocity.y);
+		}
+	}
+}
